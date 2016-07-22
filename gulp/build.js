7b2b62c21825f1ace57cf93890058bd155730461
Shin <mrmaxin@gmail.com>
2016-07-22 12:45:29 -0700

    change font filters inside build.js

--- gulp/build.js
+++ gulp/build.js
@@ -71,9 +71,11 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-  return gulp.src($.mainBowerFiles())
-    .pipe($.filter('**/*.{eot,otf,svg,ttf,woff,woff2}'))
-    .pipe($.flatten())
+  return gulp.src([
+      'bower_components/bootstrap-sass/assets/fonts/**/*',
+      'bower_components/font-awesome/fonts/*',
+    ])
+    .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
 });
 
