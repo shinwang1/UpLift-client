dd1c415994674883df34c2f4fb2b6cef517011b2
Shin <mrmaxin@gmail.com>
2016-07-22 13:09:45 -0700

    add build view task to gulp

--- gulp/build.js
+++ gulp/build.js
@@ -96,4 +96,10 @@ gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('views', function() {
+  return gulp.src([
+    path.join(conf.paths.src, '/views/**/*')
+  ]).pipe(gulp.dest(path.join(conf.paths.dist, '/views/')));
+});
+
+gulp.task('build', ['html', 'views', 'fonts', 'other']);
