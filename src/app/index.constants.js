2024acf8f069093dbd07977fbd9244e8a5101a7c
Shin <mrmaxin@gmail.com>
2016-07-22 12:07:05 -0700

    initial commit w gulp-angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('formIo')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
