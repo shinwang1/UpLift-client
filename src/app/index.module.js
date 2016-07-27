25aafbe585067f003a04f432088cb63fed6bd6cc
Shin <mrmaxin@gmail.com>
2016-07-26 21:48:05 -0700

    Update 2016-07-27T04:48:01.163Z

--- src/app/index.module.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('formIo', ['ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr', 'formio', 'ngFormioHelper']);
-
-})();
